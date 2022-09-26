46b87ae6552a8dffcff466de177d22dff7e2157d
Anietie Brownson <anni.brownson@gmail.com>
2022-09-26 13:25:25 +0100

    created build file

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import QRCode from 'qrcode'
+import React, { useState } from 'react'
+import './App.css'
+
+
+const App = () => {
+  const [ url, setUrl ] = useState('')
+  const [ qrcode, setQrcode ] = useState('')
+
+  
+  const GenerateQRCode = () => {
+    QRCode.toDataURL( url, (err, url) => {
+      if (err) return console.error(err)
+
+      console.log(url)
+      setQrcode(url)
+    } )
+  }
+
+  return (
+    <>
+      <main>
+        <h1>QR Generator</h1>
+        <input
+          type='text'
+          placeholder='e.g. https://facebook.com'
+          value={url}
+          onChange={e => setUrl(e.target.value)} />
+          
+          <div class='btn'>
+          <button onClick={GenerateQRCode}>Generate</button>
+          </div>
+
+          
+
+          {qrcode && <>
+            <img src={qrcode} alt='qr-code' />
+            <a href={qrcode} download='qr-code.png'> Download </a>
+          </> }
+      </main>
+    </>
+  )
+}
+
+export default App;
